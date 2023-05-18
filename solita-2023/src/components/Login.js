42ea48e7bfb1d8ffa9fa2d15b9cc7776d6be9616
seveneleven337 <solidxnake_6@hotmail.com>
2023-05-18 07:13:26 +0300

    added axios authentication, basic login view and added token to localStorage

--- solita-2023/src/components/Login.js
+++ solita-2023/src/components/Login.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import { authentication } from '../controllers/Data';
 
 const Login = () => {
+  
+  const [user, setUser] = useState()
+  const [password, setPassword] = useState()
+
+  async function login() {
+    await authentication(user, password)
+    .catch(console.error)
+    .then(response=>{
+      console.log(response)
+    })
+  }
+
   return (
-    <div className="App">
-        Login view
+    <div className="Login">
+        <h1>Login</h1>
+        <div>
+          <input type="text" onChange={(event) => setUser(event.target.value)} /> <br></br>
+          <input type="password" onChange={(event) => setPassword(event.target.value)} /> <br></br>
+          <button onClick={()=>{login()}}>Login</button>
+        </div>
     </div>
   );
 }
