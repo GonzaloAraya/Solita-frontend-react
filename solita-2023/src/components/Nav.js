e6eb9b8f04a9e143f4056bc9d59fccbb9a31c7ea
seveneleven337 <solidxnake_6@hotmail.com>
2023-05-22 07:57:34 +0300

    changed top right icon in Nav bar

--- solita-2023/src/components/Nav.js
+++ solita-2023/src/components/Nav.js
@@ -133,7 +133,10 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open Login">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar
+                  alt="solita"
+                  src={require("../solita.png")}
+                />
               </IconButton>
             </Tooltip>
             <Menu
