9f5ecca8ad36bab683b607e2132e2698579f8ad4
seveneleven337 <solidxnake_6@hotmail.com>
2023-05-28 10:07:47 +0300

    edit information in row table and added update axios

--- solita-2023/src/components/common/CustomTable.js
+++ solita-2023/src/components/common/CustomTable.js
@@ -1,35 +1,60 @@
-import { DataGrid } from '@mui/x-data-grid';
-import React, { useEffect, useState } from 'react'
-import { bikeJourneyData } from '../../controllers/Data';
+import { DataGrid } from "@mui/x-data-grid";
+import React, { useEffect, useState } from "react";
+import { bikeJourneyData, updateBikeJourneyData } from "../../controllers/Data";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
-
-/** 
- * 
+/**
  * DataGrid Table where data, pagination and display information is made.
- * Note: Page size limited by VM cloud service, Must be updated to work properly
- * 
+ * Row information can be edited
  * @returns DataGrid table
- * 
-*/
+ * */
 
 //columns defined in the imported data
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'departure', headerName: 'Departure', width: 200 },
-  { field: '_return', headerName: 'Return', width: 200 },
-  { field: 'departureStationId', headerName: 'Departure Station ID', width: 130 },
-  { field: 'departureStationName', headerName: 'Departure Station Name', width: 200 },
-  { field: 'returnStationId', headerName: 'Return Station ID', width: 130 },
-  { field: 'returnStationName', headerName: 'Return Station Name', width: 200 },
-  { field: 'coveredDistance', headerName: 'Covered Distance', width: 130 },
-  { field: 'duration', headerName: 'Duration', width: 130 },
+  { field: "id", headerName: "ID", width: 130 },
+  { field: "departure", headerName: "Departure", width: 200, editable: true },
+  { field: "_return", headerName: "Return", width: 200, editable: true },
+  {
+    field: "departureStationId",
+    headerName: "Departure Station ID",
+    width: 130,
+    editable: true,
+  },
+  {
+    field: "departureStationName",
+    headerName: "Departure Station Name",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "returnStationId",
+    headerName: "Return Station ID",
+    width: 130,
+    editable: true,
+  },
+  {
+    field: "returnStationName",
+    headerName: "Return Station Name",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "coveredDistance",
+    headerName: "Covered Distance",
+    width: 130,
+    editable: true,
+  },
+  { field: "duration", headerName: "Duration", width: 130, editable: true },
 ];
 
-
-//Datagrid from material UI
+//Datagrid from MUI
 export default function CustomTable() {
+  const [rows, setRows] = useState([]);
+  const [snackbar, setSnackbar] = React.useState(null);
+  const handleCloseSnackbar = () => setSnackbar(null);
 
-  const [rows, setRows] = useState([])
+  //MUI pagination footer
   const [paginationModel, setPaginationModel] = React.useState({
     pageSize: 50,
     page: 0,
@@ -38,28 +63,55 @@ export default function CustomTable() {
   //updating data only on the first render and any time paginationModel value changes
   useEffect(() => {
     readData();
-  }, [paginationModel])
+  }, [paginationModel]);
 
   //fetching data from controller, update all data information
   async function readData() {
     await bikeJourneyData(paginationModel.page, 10000)
-    .catch(console.error)
-    .then(response=>{
-      setRows(response);
-    })
+      .catch(console.error)
+      .then((response) => {
+        setRows(response);
+      });
   }
-  
+
+  //edit information inside a row cell
+  const processRowUpdate = React.useCallback((newRow) => {
+    const updatedRow = { ...newRow, isNew: false };
+    setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    setSnackbar({ children: "User successfully saved", severity: "success" });
+    updateBikeJourneyData(newRow.id, newRow);
+    console.log(newRow);
+    return updatedRow;
+  });
+
+  // error handling, notification if an error occurs
+  const handleProcessRowUpdateError = React.useCallback((error) => {
+    setSnackbar({ children: error.message, severity: "error" });
+  }, []);
+
   return (
-    <div style={{ height: 'auto', width: '90%'}}>
+    <div style={{ height: "auto", width: "90%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSizeOptions={[10,25,50]}
+        pageSizeOptions={[10, 25, 50]}
         paginationModel={paginationModel}
         onPaginationModelChange={setPaginationModel}
         {...rows}
-        checkboxSelection
+        checkboxSelection={false}
+        processRowUpdate={processRowUpdate}
+        onProcessRowUpdateError={handleProcessRowUpdateError}
       />
+      {!!snackbar && (
+        <Snackbar
+          open
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          onClose={handleCloseSnackbar}
+          autoHideDuration={6000}
+        >
+          <Alert {...snackbar} onClose={handleCloseSnackbar} />
+        </Snackbar>
+      )}
     </div>
   );
 }
